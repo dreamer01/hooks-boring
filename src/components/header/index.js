b4807ffa73796b8bba054aabd63d1cba6b4612c2
Kuliza-452 <psnitsri01@gmail.com>
2019-05-01 11:36:05 +0530

    adding header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default class Header extends Component {
-	render() {
-		return <div>Logo About</div>;
-	}
-}
+import logo from "../../assets/icons/logo.svg";
+
+const Row = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Wrapper = styled(Row)`
+	justify-content: space-between;
+	margin: 10px 0px;
+`;
+
+const Logo = styled.img`
+	height: 50px;
+`;
+
+const Title = styled.h1`
+	margin-left: 10px;
+`;
+
+const About = styled.p`
+	justify-self: flex-end;
+	font: 20px;
+	font-weight: 600;
+`;
+
+export default () => {
+	return (
+		<Wrapper>
+			<Link to="/">
+				<Row>
+					<Logo src={logo} />
+					<Title>Kya Karen</Title>
+				</Row>
+			</Link>
+			<About>About</About>
+		</Wrapper>
+	);
+};
