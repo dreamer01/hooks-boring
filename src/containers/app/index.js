40db287045019de678982a9cf8642d57c2ab0bf3
Kuliza-452 <psnitsri01@gmail.com>
2019-05-01 11:29:10 +0530

    Adding Route and Container

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import "./styles.css";
+import Home from "../home";
+import Where from "../where";
+import What from "../what";
+import Which from "../home";
 
 function App() {
-	return <div>Hey</div>;
+	return (
+		<>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/where" component={Where} />
+				<Route exact path="/what" component={What} />
+				<Route exact path="/which" component={Which} />
+			</Switch>
+		</>
+	);
 }
 
 export default App;
---
+++ src/containers/home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Header from "../../components/header";
+import "./styles.css";
+
+export default class Home extends Component {
+	render() {
+		return (
+			<>
+				<Header />
+			</>
+		);
+	}
+}
