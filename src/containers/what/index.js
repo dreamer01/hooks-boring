8ec4f9ba41d0b0225c6f0d263d132ffa7ebcf555
Kuliza-452 <psnitsri01@gmail.com>
2019-05-01 13:54:24 +0530

    Adding contentful api

--- src/containers/what/index.js
+++ src/containers/what/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import Layout from "../../components/layout";
+import Category from "../../components/category";
 import UpArrow from "../../assets/icons/up-arrow.svg";
 import DownArrow from "../../assets/icons/down-arrow.svg";
 
+var contentful = require("contentful");
 const Conatiner = styled.div`
 	flex: 1;
 	display: flex;
@@ -18,10 +20,25 @@ const Icon = styled.img`
 	height: ${props => (props.size === "small" ? "25px" : "25px")};
 `;
 
-const Category = styled.div``;
-
 export default ({ data }) => {
 	let [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		const client = contentful.createClient({
+			space: "xsej5tvgomz6",
+			accessToken:
+				"2585b2432776f2801240a4257fce8d9c9584975557ec8ae312bc5c1acc0593d6",
+		});
+
+		client
+			.getEntries({
+				content_type: "category",
+			})
+			.then(function(entries) {
+				console.log(entries.items[0].fields.featureImg.fields);
+				setCategories(entries.items);
+			});
+	}, []);
 
 	return (
 		<Layout>
@@ -31,10 +48,10 @@ export default ({ data }) => {
 			<Conatiner>
 				{categories &&
 					categories.map(category => (
-						<Link key={category.id} to="/which">
+						<Link key={category.sys.id} to="/which">
 							<Category
-								src={category.featureImg.fluid.src}
-								title={category.title}
+								src={category.fields.featureImg.fields.file.url}
+								title={category.fields.title}
 							/>
 						</Link>
 					))}
--- src/containers/which/index.js
+++ src/containers/which/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import Layout from "../../components/layout";
+import Activity from "../../components/category";
 import UpArrow from "../../assets/icons/up-arrow.svg";
 import DownArrow from "../../assets/icons/down-arrow.svg";
 
+var contentful = require("contentful");
 const Conatiner = styled.div`
 	flex: 1;
 	display: flex;
@@ -18,10 +20,25 @@ const Icon = styled.img`
 	height: ${props => (props.size === "small" ? "25px" : "25px")};
 `;
 
-const Activity = styled.div``;
-
 export default ({ data }) => {
 	const [activities, setActivities] = useState([]);
+
+	useEffect(() => {
+		const client = contentful.createClient({
+			space: "xsej5tvgomz6",
+			accessToken:
+				"2585b2432776f2801240a4257fce8d9c9584975557ec8ae312bc5c1acc0593d6",
+		});
+
+		client
+			.getEntries({
+				content_type: "activity",
+			})
+			.then(function(entries) {
+				setActivities(entries.items);
+			});
+	}, []);
+
 	return (
 		<Layout>
 			<Link to="/what">
@@ -29,10 +46,10 @@ export default ({ data }) => {
 			</Link>
 			<Conatiner>
 				{activities.map(activity => (
-					<Link key={activity.id} to="/letsgo">
+					<Link key={activity.sys.id} to="/letsgo">
 						<Activity
-							src={activity.featureImg[0].fluid.src}
-							title={activity.title}
+							src={activity.fields.featureImg[0].fields.file.url}
+							title={activity.fields.title}
 						/>
 					</Link>
 				))}
