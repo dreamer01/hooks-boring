ef45de1c715bcae3c46c33e8156685695a4d59e2
Kuliza-452 <psnitsri01@gmail.com>
2019-05-03 20:48:19 +0530

    Adding Typography

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { TypographyStyle, GoogleFont } from "react-typography";
 
+import { typography } from "./utils/typograpgy";
 import App from "./containers/app";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
 	<BrowserRouter>
+		<TypographyStyle typography={typography} />
+		<GoogleFont typography={typography} />
 		<App />
 	</BrowserRouter>,
 	document.getElementById("root")
---
+++ src/utils/typograpgy.js
@@ -0,0 +1,98 @@
+import Typography from "typography";
+
+const customTypography = {
+	baseFontSize: "16px",
+	baseLineHeight: 1.5,
+	bodyFontFamily: ["Quicksand", "Oxygen"],
+	scaleRatio: 2.25,
+	bodyWeight: 400,
+	headerWeight: 500,
+	googleFonts: [
+		{
+			name: "Raleway",
+			styles: ["700"],
+		},
+		{
+			name: "Quicksand",
+			styles: ["400", "400i", "700", "700i"],
+		},
+	],
+
+	overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+		body: {
+			color: "#000",
+			//color: "#fff",
+		},
+		p: {
+			marginBottom: 0,
+		},
+		a: {
+			color: "#1ab394",
+			textDecoration: "none",
+		},
+		h1: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(4 / 4),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		h2: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(3 / 4),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		h3: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(2 / 4),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		h4: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(1 / 6),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		h5: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(-1 / 6),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		h6: {
+			fontFamily: ["Raleway", "Oxygen"].join(","),
+			fontSize: scale(-2 / 6),
+			color: "#0D2C54",
+			marginBottom: 0,
+		},
+		blockquote: {
+			...scale(1 / 4),
+			borderLeft: `${rhythm(1 / 6)} solid #eceeef`,
+			paddingTop: rhythm(1 / 3),
+			paddingBottom: rhythm(1 / 3),
+			paddingLeft: rhythm(2 / 3),
+			paddingRight: rhythm(2 / 3),
+		},
+		"blockquote > :last-child": {
+			marginBottom: 0,
+		},
+		"blockquote cite": {
+			...adjustFontSizeTo(options.baseFontSize),
+			color: "#efefef",
+			fontWeight: options.bodyWeight,
+			fontStyle: "normal",
+		},
+	}),
+};
+
+const typography = new Typography(customTypography);
+
+// Output CSS as string.
+typography.toString();
+
+// Or insert styles directly into the <head> (works well for client-only
+// JS web apps.
+typography.injectStyles();
+
+export { typography };
