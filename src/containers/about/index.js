bbd60ed1641d8308b307c1c23a9bed972623ec5d
Kuliza-452 <psnitsri01@gmail.com>
2019-05-13 23:11:08 +0530

    Adding Google Analytics

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 
 import Layout from "../../components/layout";
 import Thinking from "../../assets/images/thinking.svg";
@@ -27,19 +28,27 @@ const Text = styled.p`
 export default function About() {
 	return (
 		<Layout>
+			<Helmet>
+				<title>About</title>
+				<meta
+					name="description"
+					content="So here is what we deliver to you with our blazing fast suggestion web app What2Do."
+				/>
+			</Helmet>
 			<ActionImg src={Thinking} alt="about" />
 			<Wrapper>
 				<Heading>What is What2Do ?</Heading>
 				<Text>
 					What2Do is a recommendation/suggestion web app where it helps you find
-					something to do in your free time. We often sit idle and think now
-					that I have time what I should do? W2D helps you answer that question.
-					What2Do helps you find an activity of your interest by four step
-					process. What2Do make sure of it that your free time is efficiently
-					spent by you while having an interest in what you do. It helps you
-					explore and discover new things to do with your time. The
-					recommendation by What2Do varies from Book, Movies mobile app to board
-					games and many more interesting activities around you.
+					something to do in your free time. We often sit idle and think what I
+					should do now that I have time. W2D helps you answer that question. It
+					helps you find an activity of your interest by four QnA selection
+					steps process. What2Do make sure of it that your free time is
+					efficiently spent by you while doing something of your interest and
+					just for sake of timepass. It helps you explore and discover new
+					things to do with your time. The recommendation by What2Do varies from
+					Book, Movies mobile app to board games and many more interesting
+					activities around you.
 				</Text>
 			</Wrapper>
 		</Layout>
--- src/containers/app/app.local.js
+++ src/containers/app/app.local.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import withTracker from "../../utils/withTracker";
 import Home from "../home";
 import Where from "../where";
 import What from "../what";
@@ -11,12 +12,12 @@ import About from "../about";
 function App() {
 	return (
 		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/where" component={Where} />
-			<Route exact path="/what" component={What} />
-			<Route exact path="/which" component={Which} />
-			<Route exact path="/letsgo" component={Letsgo} />
-			<Route exact path="/about" component={About} />
+			<Route exact path="/" component={withTracker(Home)} />
+			<Route exact path="/where" component={withTracker(Where)} />
+			<Route exact path="/what" component={withTracker(What)} />
+			<Route exact path="/which" component={withTracker(Which)} />
+			<Route exact path="/letsgo" component={withTracker(Letsgo)} />
+			<Route exact path="/about" component={withTracker(About)} />
 		</Switch>
 	);
 }
