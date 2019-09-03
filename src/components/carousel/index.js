6b1a3783c681169f062e6efaee551691e086080f
Kuliza-438 <psnitsri01@gmail.com>
2019-09-03 20:44:21 +0530

    Adding carousel component

---
+++ src/components/carousel/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import UpArrow from "../../assets/icons/up-arrow.svg";
+
+const Wrapper = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: center;
+`;
+
+const Container = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: center;
+	padding-top: 50px;
+	padding-bottom: 50px;
+	overflow-x: auto;
+	::-webkit-scrollbar {
+		display: none !important;
+	}
+	scroll-behavior: smooth;
+`;
+
+const PrevIcon = styled.img`
+	height: ${props => (props.size === "small" ? "25px" : "25px")};
+	transform: rotate(-90deg);
+	@media (max-width: 500px) {
+		display: none;
+	}
+`;
+
+const NextIcon = styled.img`
+	height: ${props => (props.size === "small" ? "25px" : "25px")};
+	transform: rotate(90deg);
+	@media (max-width: 500px) {
+		display: none;
+	}
+`;
+
+function Carousel({ children, ...props }) {
+	const handlePrev = event => {
+		event.preventDefault();
+		document.getElementById("carousel").scrollLeft -= 300;
+	};
+
+	const handleNext = event => {
+		event.preventDefault();
+		document.getElementById("carousel").scrollLeft += 300;
+	};
+
+	return (
+		<Wrapper>
+			<PrevIcon onClick={handlePrev} src={UpArrow} alt="prev" />
+			<Container id="carousel" onKeyUp={handlePrev} onKeyDown={handleNext}>
+				{children}
+			</Container>
+			<NextIcon onClick={handleNext} src={UpArrow} alt="next" />
+		</Wrapper>
+	);
+}
+
+Carousel.propTypes = {
+	children: PropTypes.any,
+};
+
+export default Carousel;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Carousel from "./carousel";
+import Category from "./category";
+import Footer from "./footer";
+import Header from "./header";
+import Layout from "./layout";
+import Loader from "./loader";
+
+export {
+    Carousel,
+    Category,
+    Footer,
+    Header,
+    Layout,
+    Loader
+}
