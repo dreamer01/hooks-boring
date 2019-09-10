7ceb4d72d79c17d35161a854ea16fbde1666d0d7
Kuliza-438 <psnitsri01@gmail.com>
2019-09-10 23:32:42 +0530

    Adding Details to Activity Page

--- src/containers/letsgo/index.js
+++ src/containers/letsgo/index.js
@@ -4,6 +4,9 @@ import Helmet from "react-helmet";
 
 import client from "../../utils/contentful";
 import { Layout, Loader } from "../../components";
+import ExternalLink from "../../assets/icons/external-link.svg";
+import User from "../../assets/icons/user.svg";
+import Twitter from "../../assets/icons/twitter.svg";
 
 const Conatiner = styled.div`
 	flex: 1;
@@ -14,12 +17,47 @@ const Conatiner = styled.div`
 	justify-content: space-around;
 `;
 
+const Row = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Details = styled.div`
+	display: flex;
+`;
+
+const Info = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: flex-start;
+	width: 50%;
+`;
+
 const FeatureImg = styled.img`
-	width: 80%;
-	height: 150px;
-	border-radius: 5px;
+	width: 40%;
+	border-radius: 2px;
 	object-fit: cover;
-	margin: 20px 0px;
+`;
+
+const Title = styled.h3`
+	align-self: left;
+`;
+
+const LinkIcon = styled.img`
+	height: ${props => (props.size === "small" ? "15px" : "25px")};
+	margin-left: 10px;
+`;
+
+const TagsGroup = styled.div`
+	display: flex;
+`;
+
+const Tag = styled.div`
+	padding: 5px 10px;
+	margin-right: 10px;
+	border: 2px solid teal;
+	border-radius: 50px;
 `;
 
 export default ({ data }) => {
@@ -35,6 +73,12 @@ export default ({ data }) => {
 			.catch(error => console.log(error));
 	}, [activity.sys.id]);
 
+	const renderTags = tag => (
+		<Tag key={tag}>
+			<pre>{tag.toUpperCase()}</pre>
+		</Tag>
+	);
+
 	return (
 		<Layout>
 			<Helmet>
@@ -44,8 +88,38 @@ export default ({ data }) => {
 			<Conatiner>
 				{selected ? (
 					<>
-						<FeatureImg src={selected.fields.featureImg[0].fields.file.url} />
-						<p>{selected.fields.description}</p>{" "}
+						<div>
+							<Details>
+								<div>
+									<FeatureImg
+										src={selected.fields.featureImg[0].fields.file.url}
+									/>
+								</div>
+								<Info>
+									<Row>
+										<h3> Used By {selected.fields.usedBy}</h3>
+										<LinkIcon size="small" src={User} alt="User" />
+									</Row>
+									<Row>
+										<a
+											target="_blank"
+											rel="noopener noreferrer"
+											href={selected.fields.links[0]}
+										>
+											<h3>Try Now</h3>
+										</a>
+										<LinkIcon size="small" src={ExternalLink} alt="Open Link" />
+									</Row>
+									<Row>
+										<h3>Spread Fun</h3>
+										<LinkIcon src={Twitter} alt="Tweet" />
+									</Row>
+									<TagsGroup>{selected.fields.tags.map(renderTags)}</TagsGroup>
+								</Info>
+							</Details>
+							<Title>{selected.fields.title}</Title>
+						</div>
+						<p>{selected.fields.description}</p>
 					</>
 				) : (
 					<Loader />
